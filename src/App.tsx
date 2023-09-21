bfa078b0f8a1f8718e874660aea111dfb0b0690e
vendredl <node.js_@kakao.com>
2023-09-21 22:17:17 +0900

    :lipstick: Darkmode 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
-import DetailPage from './pages/DetailPage';
-import HomePage from './pages/HomePage';
+import { createBrowserRouter, RouterProvider, useRouteError, } from 'react-router-dom';
+import DetailPage from './pages/DetailPage/DetailPage';
+import HomePage from './pages/HomePage/HomePage';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { darkMode } from './utils/atoms';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './utils/themes';
+import {styled} from 'styled-components'
+import { MdLightMode as LightModeBtn, MdModeNight as NightModeBtn } from "react-icons/md";
+import { GlobalStyle } from './styles/styles';
+// import ErrorBoundary from './utils/ErrorBoundary';
 
+const DarkModeBtn = styled.div`
+    color: ${(props) => props.theme.text};
+    background: ${(props) => props.theme.body};
+    font-size: 1.5rem;
+    border-radius: 100%;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 10px;
+    right: 10px;
+`
 
 function App() {
+
+
+  // const isdarkmode = useRecoilValue(darkMode);
+  const [isdarkmode, setisDarkMode] = useRecoilState(darkMode);
 
   const router = createBrowserRouter([
     {
@@ -17,8 +43,16 @@ function App() {
     },
   ]);
 
+  
+
   return (
-    <RouterProvider router={router} />
+      <ThemeProvider theme={isdarkmode ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+      <DarkModeBtn onClick={() => setisDarkMode(!isdarkmode)} >
+            {isdarkmode ? <LightModeBtn /> : <NightModeBtn />}
+      </DarkModeBtn>
+    </ThemeProvider>
   );
 }
 
