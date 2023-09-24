6cb4510e3220a20caacca1d604761061f998eebb
vendredl <node.js_@kakao.com>
2023-09-24 14:10:29 +0900

    :wastebasket: 필요없는 import 제거

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchCharacterDetail } from '../../api/api';
 import styled from 'styled-components'
